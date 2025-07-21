a92ded9694637b498efadaa307345797a5cc0ce7
ByteGab <gabrielfreiresimon@gmail.com>
2025-07-21 10:30:52 +0200

    add librarys

--- minishell/minishell.h
+++ minishell/minishell.h
@@ -3,26 +3,31 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gafreire <gafreire@student.42.fr>          #+#  +:+       +#+        */
+/*   By: gafreire <gafreire@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025-07-08 14:10:34 by gafreire          #+#    #+#             */
-/*   Updated: 2025-07-08 14:10:34 by gafreire         ###   ########.fr       */
+/*   Created: 2025/07/08 14:10:34 by gafreire          #+#    #+#             */
+/*   Updated: 2025/07/21 10:22:50 by gafreire         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
-#include <stdio.h>
-#include <readline/readline.h> 
-#include <readline/history.h>
+# include <readline/history.h>
+# include <readline/readline.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <unistd.h>
 
-typedef enum Tokens 
+typedef enum Tokens
 {
-    COMMAND,
-    ARGUMENT,
-    OPERATORS,
-    VARIABLES
-} Tokens;
+	COMMAND,
+	ARGUMENT,
+	OPERATORS,
+	VARIABLES
+}	Tokens;
 
-# endif
+int	check_token(int argc, char *argv[]);
+
+#endif
