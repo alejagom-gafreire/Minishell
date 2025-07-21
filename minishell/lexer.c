55af6ad699a12f629da31d00936a856205ad2780
ByteGab <gabrielfreiresimon@gmail.com>
2025-07-21 15:24:21 +0200

    add file lexer

---
+++ minishell/lexer.c
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   lexer.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gafreire <gafreire@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/21 12:48:14 by gafreire          #+#    #+#             */
+/*   Updated: 2025/07/21 13:45:40 by gafreire         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+/* 
+Check the line character by character to find the special operators
+*/
+void	check_line(char *line) // change type variable
+{
+	int i;
+
+	i = 0;
+
+	while (line[i] != '\0')
+	{
+        if (line[i] == '|')
+        {
+		    printf("Encontre un: %c\n", line[i]);
+        }
+        else if (line[i] == '<')
+        {
+		    printf("Encontre un: %c\n", line[i]);
+        }
+        else if (line[i] == '>')
+        {
+            if (line[i + 1] == '>')
+            {
+		        printf("Encontre uno doble: %c%c\n", line[i],line[i + 1]);
+                i++;
+            }
+            else
+		        printf("Encontre uno simple: %c\n", line[i]);
+                
+        }
+		// printf("%c", line[i]);
+		i++;
+	}
+	printf("\n");
+}
