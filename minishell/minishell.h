8d45ecd90564da104a8db6ed0bd2457fb78b9459
ByteGab <gabrielfreiresimon@gmail.com>
2025-07-21 19:25:44 +0200

    add structs lexer and parcer

--- minishell/minishell.h
+++ minishell/minishell.h
@@ -6,7 +6,7 @@
 /*   By: gafreire <gafreire@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/08 14:10:34 by gafreire          #+#    #+#             */
-/*   Updated: 2025/07/21 10:22:50 by gafreire         ###   ########.fr       */
+/*   Updated: 2025/07/21 19:16:37 by gafreire         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,12 +22,33 @@
 
 typedef enum Tokens
 {
-	COMMAND,
-	ARGUMENT,
-	OPERATORS,
-	VARIABLES
-}	Tokens;
+	T_NAME_CMD, // grep ls echo etc....
+	T_PIPE, // |
+	T_REDIR_IN, // < , << simbolos.
+	T_REDIR_OUT, // > >> simbolos.
+	T_INFILE, //primer txt.
+	T_OUTFILE, //salida del txt ultimo.
+	T_GENERAL //otras cosas.
+}		Tokens;
 
-int	check_token(int argc, char *argv[]);
+typedef struct s_lexer
+{
+	int	id;
+	char *inf;
+	enum tokens;
+	t_lexer *next;
+	t_lexer *last;
+}		t_lexer;
+
+typedef struct s_parcer
+{
+	char *cmd_args;
+	char *name_infile;
+	char *name_outfile;
+	t_parcer *next;
+}		t_parcer;
+
+int		check_token(int argc, char *argv[]);
+void	check_line(char *line);
 
 #endif
