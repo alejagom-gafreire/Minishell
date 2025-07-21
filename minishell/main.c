76055ee90732cd4abaefdccc06e5bb0391bb0249
ByteGab <gabrielfreiresimon@gmail.com>
2025-07-21 12:16:54 +0200

    change printf

--- minishell/main.c
+++ minishell/main.c
@@ -6,7 +6,7 @@
 /*   By: gafreire <gafreire@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/08 14:09:27 by gafreire          #+#    #+#             */
-/*   Updated: 2025/07/21 10:22:27 by gafreire         ###   ########.fr       */
+/*   Updated: 2025/07/21 12:16:46 by gafreire         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,6 @@ int main (int argc, char *argv[])
     if (argc == 1)
         check_token(argc,argv);
     else
-        printf("Esta mal");
+        printf("Esta mal\n");
     return (0);
 }
