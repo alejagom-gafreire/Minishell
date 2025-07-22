132ecadf03d87e1289c2a7dac335db64f0decd0e
ByteGab <gabrielfreiresimon@gmail.com>
2025-07-22 13:48:57 +0200

    add token T_NAME_CMD and T_GENERAL

--- minishell/lexer.c
+++ minishell/lexer.c
@@ -6,7 +6,7 @@
 /*   By: gafreire <gafreire@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/21 12:48:14 by gafreire          #+#    #+#             */
-/*   Updated: 2025/07/22 10:16:37 by gafreire         ###   ########.fr       */
+/*   Updated: 2025/07/22 13:04:39 by gafreire         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,31 +51,95 @@ void	check_line(char *line) // change type variable
 {
 	int i;
 	t_lexer *lexer_list = NULL;
+	int first_word = 1;
+	tokens last_token = T_GENERAL;
 	i = 0;
 
 	while (line[i] != '\0')
 	{
-		if (line[i] == '|')
+		if (line[i] == ' ')
+		{
+			i++;
+		}
+		else if (line[i] == '|')
+		{
 			add_token(&lexer_list, "|", T_PIPE);
+			last_token = T_PIPE;
+			first_word = 1; // despues de un pipe siempre viene un comando
+			i++;
+		}
 		else if (line[i] == '<')
 		{
-			add_token(&lexer_list, "<", T_REDIR_IN);
-			// tambien añadir <<
+			if (line[i + 1] == '<')
+			{
+				add_token(&lexer_list, "<<", T_REDIR_IN);
+				last_token = T_REDIR_IN;
+				i += 2;
+			}
+			else
+			{
+				add_token(&lexer_list, "<", T_REDIR_IN);
+				last_token = T_REDIR_IN;
+				i++;
+			}
 		}
 		else if (line[i] == '>')
 		{
 			if (line[i + 1] == '>')
 			{
 				add_token(&lexer_list, ">>", T_REDIR_OUT);
-				i++;
+				last_token = T_REDIR_OUT;
+				i += 2;
 			}
 			else
+			{
 				add_token(&lexer_list, ">", T_REDIR_OUT);
+				last_token = T_REDIR_OUT;
+				i++;
+			}
 		}
-		// añadir para T_GENERAL
-		// printf("%c", line[i]);
-		i++;
+		else
+		{
+			int start = i;
+			while (line[i] != '\0' && line[i] != ' ' && line[i] != '<'
+				&& line[i] != '>' && line[i] != '|')
+				i++;
+			if (i > start)
+			{
+				char *word = strndup(&line[start], i - start);
+					// cambiar por ft_substr de la libft??
+				if (first_word)                               
+					// Si es la primera palabra
+				{
+					add_token(&lexer_list, word, T_NAME_CMD);
+					first_word = 0; // ya no es la primera palabra
+					last_token = T_NAME_CMD;
+				}
+				else if (last_token == T_REDIR_IN)
+					// Si viene de <,entonces T_INFILE
+				{
+					add_token(&lexer_list, word, T_INFILE);
+					last_token = T_INFILE;
+				}
+				else if (last_token == T_REDIR_OUT)
+					// Si viene de > o >> entonces T_OUTFILE
+				{
+					add_token(&lexer_list, word, T_OUTFILE);
+					last_token = T_OUTFILE;
+				}
+				else
+				{
+					add_token(&lexer_list, word, T_GENERAL);
+					last_token = T_GENERAL;
+				}
+			}
+		}
 	}
+	// añadir para T_NAME_CMD Acces??
+	// añadir para T_INFILE
+	// añadir para T_OUTFILE
+	// añadir para T_GENERAL
+	// printf("%c", line[i]);
 	printf("\n");
 	print_tokens(lexer_list);
-}
+}  
