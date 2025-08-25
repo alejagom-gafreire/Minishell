75a777a4881b56f03d38fc0794aad2f2eda9f442
ByteGab <gabrielfreiresimon@gmail.com>
2025-08-25 11:31:24 +0200

    add control built-ins

--- minishell/src/built-ins/built-ins.c
+++ minishell/src/built-ins/built-ins.c
@@ -3,6 +3,64 @@
 /*                                                        :::      ::::::::   */
 /*   built-ins.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
+/*   By: gafreire <gafreire@student.42.fr>          #+#  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025-08-23 15:31:29 by gafreire          #+#    #+#             */
+/*   Updated: 2025-08-23 15:31:29 by gafreire         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+/*
+	Lo separo en dos funciones para que en el ejecutor puedas 
+	checkear de manera rapida si es un builtin y si lo es ejecutarlo
+
+	exec_builtins debería devolver el valor del status
+*/
+
+void exec_builtins(char *builtin)
+{
+
+	if (!ft_strcmp(builtin, "echo"))
+		exec_echo();	
+	// else if (!ft_strcmp(builtin, "cd"))
+		// exec_cd
+	// else if (!ft_strcmp(builtin, "pwd"))
+		//exec_pwd
+	// else if (!ft_strcmp(builtin, "export"))
+		//exec_export
+	// else if (!ft_strcmp(builtin, "unset"))
+		//exec_unset
+	// else if (!ft_strcmp(builtin, "env"))
+		//exec_env
+	// else if (!ft_strcmp(builtin, "exit"))
+		//exec_exit
+}
+
+int is_builtin(char *builtin)
+{
+	if (!ft_strcmp(builtin, "echo"))
+		return (1);
+	else if (!ft_strcmp(builtin, "cd"))
+		return (1);
+	else if (!ft_strcmp(builtin, "pwd"))
+		return (1);
+	else if (!ft_strcmp(builtin, "export"))
+		return (1);
+	else if (!ft_strcmp(builtin, "unset"))
+		return (1);
+	else if (!ft_strcmp(builtin, "env"))
+		return (1);
+	else if (!ft_strcmp(builtin, "exit"))
+		return (1);
+	return (0);
+}
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   built-ins.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
 /*   By: alejandro <marvin@42.fr>                   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/29 16:43:45 by alejandro         #+#    #+#             */
