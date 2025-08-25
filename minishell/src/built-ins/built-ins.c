6cac83218a1c6b6a5ba288fbc01924e218bf26fd
ByteGab <gabrielfreiresimon@gmail.com>
2025-08-25 11:31:24 +0200

    add control built-ins

--- minishell/src/built-ins/built-ins.c
+++ minishell/src/built-ins/built-ins.c
@@ -3,35 +3,56 @@
 /*                                                        :::      ::::::::   */
 /*   built-ins.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: alejandro <marvin@42.fr>                   +#+  +:+       +#+        */
+/*   By: gafreire <gafreire@student.42.fr>          #+#  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/08/29 16:43:45 by alejandro         #+#    #+#             */
-/*   Updated: 2025/08/29 16:44:11 by alejandro        ###   ########.fr       */
+/*   Created: 2025-08-23 15:31:29 by gafreire          #+#    #+#             */
+/*   Updated: 2025-08-23 15:31:29 by gafreire         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-t_tokens	compare_buildings(char *word)
-{
-	t_tokens	type;
+/*
+	Lo separo en dos funciones para que en el ejecutor puedas 
+	checkear de manera rapida si es un builtin y si lo es ejecutarlo
 
-	(void)word;
-	if (ft_strcmp("cd", word) == 0)
-		type = T_BUILDINGS;
-	else if (ft_strcmp("echo", word) == 0)
-		type = T_BUILDINGS;
-	else if (ft_strcmp("pwd", word) == 0)
-		type = T_BUILDINGS;
-	else if (ft_strcmp("export", word) == 0)
-		type = T_BUILDINGS;
-	else if (ft_strcmp("unset", word) == 0)
-		type = T_BUILDINGS;
-	else if (ft_strcmp("env", word) == 0)
-		type = T_BUILDINGS;
-	else if (ft_strcmp("exit", word) == 0)
-		type = T_BUILDINGS;
-	else
-	    type = T_NAME_CMD;
-	return (type);
+	exec_builtins debería devolver el valor del status
+*/
+
+void exec_builtins(char *builtin)
+{
+
+	if (!ft_strcmp(builtin, "echo"))
+		exec_echo();	
+	// else if (!ft_strcmp(builtin, "cd"))
+		// exec_cd
+	// else if (!ft_strcmp(builtin, "pwd"))
+		//exec_pwd
+	// else if (!ft_strcmp(builtin, "export"))
+		//exec_export
+	// else if (!ft_strcmp(builtin, "unset"))
+		//exec_unset
+	// else if (!ft_strcmp(builtin, "env"))
+		//exec_env
+	// else if (!ft_strcmp(builtin, "exit"))
+		//exec_exit
+}
+
+int is_builtin(char *builtin)
+{
+	if (!ft_strcmp(builtin, "echo"))
+		return (1);
+	else if (!ft_strcmp(builtin, "cd"))
+		return (1);
+	else if (!ft_strcmp(builtin, "pwd"))
+		return (1);
+	else if (!ft_strcmp(builtin, "export"))
+		return (1);
+	else if (!ft_strcmp(builtin, "unset"))
+		return (1);
+	else if (!ft_strcmp(builtin, "env"))
+		return (1);
+	else if (!ft_strcmp(builtin, "exit"))
+		return (1);
+	return (0);
 }
